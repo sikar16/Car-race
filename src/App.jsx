a5bd9b0b93a0938aa6cbb383a6cf44455ecde260
sikar16 <mihiretut21@gmail.com>
2025-07-16 15:01:08 +0300

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-yellow-500'>
+      helloo
+    </div>
+  );
+}
+
+export default App
