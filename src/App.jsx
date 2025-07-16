23d49019027d375a973d9cac796990646ed4e5a9
sikar16 <mihiretut21@gmail.com>
2025-07-16 15:27:53 +0300

    prepareing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './page/Login';
+import Signup from './page/Signup';
 
 function App() {
-
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-yellow-500'>
-      helloo
-    </div>
+    <Router>
+      <div className='flex flex-col items-center justify-center h-screen bg-yellow-500'>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ src/page/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignupForm from '../componets/SignupForm'
+function Signup() {
+    return (
+        <div>
+            <SignupForm />
+        </div>
+    )
+}
+
+export default Signup
